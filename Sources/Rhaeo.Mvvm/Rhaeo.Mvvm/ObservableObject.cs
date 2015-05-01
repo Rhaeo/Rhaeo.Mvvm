213bd6f4540e9844fd9503c2635cd9a3f00ceb0b
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-05-01 16:15:12 +0200

    Implement extracting the property name

--- Sources/Rhaeo.Mvvm/Rhaeo.Mvvm/ObservableObject.cs
+++ Sources/Rhaeo.Mvvm/Rhaeo.Mvvm/ObservableObject.cs
@@ -1,11 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObservableObject.cs" company="Rhaeo">
+//   Licenced under the MIT licence.
+// </copyright>
+// <summary>
+//   Defines the ObservableObject type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
 
 namespace Rhaeo.Mvvm
 {
-    public class ObservableObject
+  /// <summary>
+  /// Serves as a back class for object instances obserable through the <see cref="INotifyPropertyChanged"/> interface.
+  /// </summary>
+  public class ObservableObject
+    : INotifyPropertyChanged
+  {
+    #region Events
+
+    /// <summary>
+    /// Occurs when a property value changes.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Raises the <see cref="PropertyChanged"/> event with a property name extracted from the property expression.
+    /// </summary>
+    /// <typeparam name="TViewModel">
+    /// The view model type.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    /// The property value type.
+    /// </typeparam>
+    /// <param name="propertyExpression">
+    /// The property expression.
+    /// </param>
+    protected void RaisePropertyChanged<TViewModel, TValue>(Expression<Func<TViewModel, TValue>> propertyExpression) where TViewModel : ObservableObject
     {
+      var propertyName = (propertyExpression.Body as MemberExpression).Member.Name;
+
+      var propertyChangedHandler = this.PropertyChanged;
+      if (propertyChangedHandler != null)
+      {
+        propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
+      }
     }
+
+    #endregion
+  }
 }
