07a26e7733d071b0834f58fead001f7e2dd15c28
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-05-01 16:51:57 +0200

    Add tests for the property raising implementation

---
+++ Sources/Rhaeo.Mvvm/Rhaeo.Mvvm.Tests/ObservableObjectTests.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObservableObjectTests.cs" company="Rhaeo">
+//   Licenced under the MIT licence.
+// </copyright>
+// <summary>
+//   Contains tests for the <see cref="ObservableObject" /> class implementation.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Rhaeo.Mvvm.Tests.ViewModels;
+
+namespace Rhaeo.Mvvm.Tests
+{
+  /// <summary>
+  /// Proves correctness of the <see cref="ObservableObject"/> class implementation.
+  /// </summary>
+  [TestClass]
+  public class ObservableObjectTests
+  {
+    #region Methods
+
+    /// <summary>
+    /// Tests the <see cref="ObservableObject.PropertyChanged"/> event being called with a correct property name extracted from the property expression.
+    /// </summary>
+    [TestMethod]
+    public void TestPropertyChangedPropertyNameFromPropertyExpressionExtraction()
+    {
+      var propertyChangedInvocations = new List<PropertyChangedEventArgs>();
+      var viewModel = new TestViewModel();
+      viewModel.PropertyChanged += (sender, e) => propertyChangedInvocations.Add(e);
+
+      viewModel.FirstName = "Tomáš";
+      viewModel.LastName = "Hübelbauer";
+      viewModel.BirthDateAndTime = new DateTime(1992, 12, 17);
+
+      CollectionAssert.AreEqual(new[] { "FirstName", "LastName", "BirthDateAndTime" }, propertyChangedInvocations.Select(propertyChangedEventArgs => propertyChangedEventArgs.PropertyName).ToArray());
+    }
+
+    /// <summary>
+    /// Tests the <see cref="ObservableObject.PropertyChanged"/> event not being called when passing an equal value to the property's current value to the property.
+    /// </summary>
+    [TestMethod]
+    public void TestPropertyChangedEventNotCalledOnEqualValue()
+    {
+      var propertyChangedInvocations = new List<PropertyChangedEventArgs>();
+      var viewModel = new TestViewModel();
+      viewModel.PropertyChanged += (sender, e) => propertyChangedInvocations.Add(e);
+
+      // Fires for FirstName, LastName and that's it.
+      viewModel.FirstName = "Tomáš";
+      viewModel.LastName = "Hübelbauer";
+      viewModel.FirstName = "Tomáš";
+
+      var viewModel2 = new TestViewModel("Tomáš", "Hübelbauer", new DateTime(1992, 12, 17));
+      viewModel2.PropertyChanged += (sender, e) => propertyChangedInvocations.Add(e);
+
+      // Doesn't fire at all since the current value of the FirstName property was initialized through the constructor.
+      viewModel2.FirstName = "Tomáš";
+
+      CollectionAssert.AreEqual(new[] { "FirstName", "LastName" }, propertyChangedInvocations.Select(propertyChangedEventArgs => propertyChangedEventArgs.PropertyName).ToArray());
+    }
+
+    #endregion
+  }
+}
---
+++ Sources/Rhaeo.Mvvm/Rhaeo.Mvvm.Tests/ViewModels/TestViewModel.cs
@@ -0,0 +1,140 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TestViewModel.cs" company="Rhaeo">
+//   Licenced under the MIT licence.
+// </copyright>
+// <summary>
+//   Represents a view model holding test data in properties whose property names are extracted from property expressions using the <see cref="ObservableObject" /> implementation.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Rhaeo.Mvvm.Tests.ViewModels
+{
+  /// <summary>
+  /// Represents a view model holding test data in properties whose property names are extracted from property expressions using the <see cref="ObservableObject"/> implementation.
+  /// </summary>
+  public sealed class TestViewModel
+    : ObservableObject
+  {
+    #region Fields
+
+    /// <summary>
+    /// Holds the first name value.
+    /// </summary>
+    private String firstName;
+
+    /// <summary>
+    /// Holds the last name value.
+    /// </summary>
+    private String lastName;
+
+    /// <summary>
+    /// Holds the birth date and time value.
+    /// </summary>
+    private DateTime bithDateAndTime;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestViewModel"/> class.
+    /// </summary>
+    public TestViewModel()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestViewModel"/> class given the full name and date of birth.
+    /// </summary>
+    /// <param name="firstName">
+    /// The first name.
+    /// </param>
+    /// <param name="lastName">
+    /// The last name.
+    /// </param>
+    /// <param name="birthDateAndTime">
+    /// The birth date and time.
+    /// </param>
+    public TestViewModel(String firstName, String lastName, DateTime birthDateAndTime)
+    {
+      this.FirstName = firstName;
+      this.LastName = lastName;
+      this.BirthDateAndTime = birthDateAndTime;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the first name.
+    /// </summary>
+    public String FirstName
+    {
+      get
+      {
+        return this.firstName;
+      }
+
+      set
+      {
+        if (this.firstName == value)
+        {
+          return;
+        }
+
+        this.firstName = value;
+        this.RaisePropertyChanged<TestViewModel, String>(testViewModel => testViewModel.FirstName);
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets the last name.
+    /// </summary>
+    public String LastName
+    {
+      get
+      {
+        return this.lastName;
+      }
+
+      set
+      {
+        if (this.lastName == value)
+        {
+          return;
+        }
+
+        this.lastName = value;
+        this.RaisePropertyChanged<TestViewModel, String>(testViewModel => testViewModel.LastName);
+
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets the birth date and time.
+    /// </summary>
+    public DateTime BirthDateAndTime
+    {
+      get
+      {
+        return this.bithDateAndTime;
+      }
+
+      set
+      {
+        if (this.bithDateAndTime == value)
+        {
+          return;
+        }
+
+        this.bithDateAndTime = value;
+        this.RaisePropertyChanged<TestViewModel, DateTime>(testViewModel => testViewModel.BirthDateAndTime);
+      }
+    }
+
+    #endregion
+  }
+}
