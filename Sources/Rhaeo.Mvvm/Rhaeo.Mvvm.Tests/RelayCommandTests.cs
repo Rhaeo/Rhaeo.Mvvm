c12f62100c6ba1ce7d2a9daa303a48c0408bc483
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-05-09 21:10:45 +0200

    Add adapter RelayCommand and tests
    This RelayCommand implementation is based on the file created as a part
    of the helper infrastructure scaffolded by Visual Studio when adding a
    new non-Blank page. Generic variant will be added following more
    re-factoring of this class. Closes #5.

---
+++ Sources/Rhaeo.Mvvm/Rhaeo.Mvvm.Tests/RelayCommandTests.cs
@@ -0,0 +1,85 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RelayCommandTests.cs" company="Rhaeo">
+//   Licenced under the MIT licence.
+// </copyright>
+// <summary>
+//   Proves correctness of the <see cref="RelayCommand" /> class implementation.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Rhaeo.Mvvm.Tests
+{
+  /// <summary>
+  /// Proves correctness of the <see cref="RelayCommand"/> class implementation.
+  /// </summary>
+  [TestClass]
+  public sealed class RelayCommandTests
+  {
+    #region Methods
+
+    /// <summary>
+    /// Test the <see cref="RelayCommand.Execute"/> method only being callable when <see cref="RelayCommand.CanExecute"/> is positive.
+    /// </summary>
+    [TestMethod]
+    public void TestRelayCommandExecuteOnlyWorkWhenCanExecuteReturnsTrue()
+    {
+      var results = new List<Boolean>();
+
+      var raiseCanExecuteChanged = default(Action);
+      var canExecute = default(Boolean);
+      var relayCommand = new RelayCommand(() => results.Add(canExecute), out raiseCanExecuteChanged, () => canExecute);
+
+      canExecute = false;
+      relayCommand.Execute(null);
+      canExecute = true;
+      relayCommand.Execute(null);
+      canExecute = false;
+      relayCommand.Execute(null);
+      canExecute = true;
+      relayCommand.Execute(null);
+
+      CollectionAssert.AreEqual(new[] { true, true }, results);
+
+      var invoked = false;
+      relayCommand.CanExecuteChanged += (sender, e) => { invoked = true; };
+      raiseCanExecuteChanged();
+
+      Assert.IsTrue(invoked);
+    }
+
+    /// <summary>
+    /// Tests <see cref="RelayCommand.CanExecuteChanged"/> being called after calling the provided predicate status updating action.
+    /// </summary>
+    [TestMethod]
+    public void TestRelayCommandCanExecuteChangedIsRaisedByTheReturnedAction()
+    {
+      var results = new List<Boolean>();
+
+      var raiseCanExecuteChanged = default(Action);
+      var canExecute = default(Boolean);
+      var relayCommand = new RelayCommand(() => results.Add(canExecute), out raiseCanExecuteChanged, () => canExecute);
+      relayCommand.CanExecuteChanged += (sender, e) => { results.Add(canExecute); };
+
+      canExecute = false;
+      raiseCanExecuteChanged();
+      relayCommand.Execute(null);
+      canExecute = true;
+      raiseCanExecuteChanged();
+      relayCommand.Execute(null);
+      canExecute = false;
+      raiseCanExecuteChanged();
+      relayCommand.Execute(null);
+      canExecute = true;
+      raiseCanExecuteChanged();
+      relayCommand.Execute(null);
+
+      CollectionAssert.AreEqual(new[] { false, true, true, false, true, true }, results);
+    }
+
+    #endregion
+  }
+}
---
+++ Sources/Rhaeo.Mvvm/Rhaeo.Mvvm/RelayCommand.cs
@@ -0,0 +1,127 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RelayCommand.cs" company="Rhaeo">
+//   Licenced under the MIT licence.
+// </copyright>
+// <summary>
+//   A command whose sole purpose is to relay its functionality
+//   to other objects by invoking delegates.
+//   The default return value for the CanExecute method is 'true'.
+//   <see cref="RaiseCanExecuteChanged" /> needs to be called whenever
+//   <see cref="CanExecute" /> is expected to return a different value.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Rhaeo.Mvvm
+{
+  /// <summary>
+  /// A command whose sole purpose is to relay its functionality 
+  /// to other objects by invoking delegates. 
+  /// The default return value for the CanExecute method is 'true'.
+  /// <see cref="RaiseCanExecuteChanged"/> needs to be called whenever
+  /// <see cref="CanExecute"/> is expected to return a different value.
+  /// </summary>
+  /// <seealso cref="http://go.microsoft.com/fwlink/?LinkID=390556">
+  /// Adapted from the Basic Page item template in Visual Studio Windows Phone application project menu.
+  /// </seealso>
+  public class RelayCommand
+    : ICommand
+  {
+    #region Fields
+
+    /// <summary>
+    /// Holds the <see cref="Execute"/> method value.
+    /// </summary>
+    private readonly Action execute;
+
+    /// <summary>
+    /// Holds the <see cref="CanExecute"/> method value.
+    /// </summary>
+    private readonly Func<Boolean> canExecute;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RelayCommand"/> class given the execution delegate and predicate.
+    /// </summary>
+    /// <param name="execute">
+    /// The execution delegate.
+    /// </param>
+    /// <param name="raiseCanExecuteChanged">
+    /// The delegate used by the command owner to notify about a predicate status update.
+    /// </param>
+    /// <param name="canExecute">
+    /// The execution predicate.
+    /// </param>
+    public RelayCommand(Action execute, out Action raiseCanExecuteChanged, Func<Boolean> canExecute = null)
+    {
+      if (execute == null)
+      {
+        throw new ArgumentNullException("execute");
+      }
+
+      this.execute = execute;
+      this.canExecute = canExecute;
+
+      raiseCanExecuteChanged = () =>
+      {
+        var canExecuteChangedHandler = this.CanExecuteChanged;
+        if (canExecuteChangedHandler != null)
+        {
+          canExecuteChangedHandler(this, EventArgs.Empty);
+        }
+      };
+    }
+
+    #endregion
+
+    #region Events
+
+    /// <summary>
+    /// Raised when RaiseCanExecuteChanged is called.
+    /// </summary>
+    public event EventHandler CanExecuteChanged;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Determines whether this <see cref="RelayCommand"/> can execute in its current state.
+    /// </summary>
+    /// <param name="parameter">
+    /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+    /// </param>
+    /// <returns>true
+    /// <code>true</code>, if this command can be executed; otherwise, <code>false</code>.
+    /// </returns>
+    public Boolean CanExecute(Object parameter)
+    {
+      return this.canExecute == null || this.canExecute();
+    }
+
+    /// <summary>
+    /// Executes the <see cref="RelayCommand"/> on the current command target.
+    /// </summary>
+    /// <param name="parameter">
+    /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+    /// </param>
+    public void Execute(Object parameter)
+    {
+      if (this.CanExecute(parameter))
+      {
+        this.execute();
+      }
+    }
+
+    #endregion
+  }
+}
